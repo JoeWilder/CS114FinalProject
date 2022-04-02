cec64c8945c3b7059e99da8c60dbe77faab1a5d6
JoeWilder <94562379+JoeWilder@users.noreply.github.com>
2022-04-02 15:32:38 -0400

    Use new form for webscraping

--- CS114FinalProject/Form1.cs
+++ CS114FinalProject/Form1.cs
@@ -8,10 +8,6 @@ namespace CS114FinalProject
 {
     public partial class Form1 : Form
     {
-        string courseData; // Raw data about a course
-        List<SNHUcourse> courseList = new List<SNHUcourse>(); // List of SNHU courses
-        int pageNumber = 0; // What page of the SNHU website we are on
-
 
         public Form1()
         {
@@ -26,93 +22,14 @@ namespace CS114FinalProject
             Logic.initRelevantTable();
             Logic.courseCompare();
             Logic.PrintCompatTable();
-
-            mySNHULogin();
-            webBrowser1.Show();
         }
 
 
-        /* Listen for page load event and check if we are on the correct page */
-        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        /* Start new web browser window */
+        private void classDataButton_Click(object sender, EventArgs e)
         {
-            pageNumber++;
-            if (pageNumber == 5)
-            {
-                navigateToCourses();
-            }
-
-            else if (pageNumber == 7)
-            {
-                navigateToCompSciMajor();
-            }
-        }
-
-
-        /* Navigate to mysnhu website and attempt to login using the given credentials */
-        private void mySNHULogin()
-        {
-            webBrowser1.Navigate("https://my.snhu.edu/");
-            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
-            {
-                Application.DoEvents();
-            }
-
-            webBrowser1.Document.GetElementById("input_1").InnerText = Environment.GetEnvironmentVariable("SNHU_EMAIL"); // Set this equal to your email (string)
-            webBrowser1.Document.GetElementById("input_2").InnerText = Environment.GetEnvironmentVariable("SNHU_PASS"); // Set this equal to your password (string)
-            webBrowser1.Document.GetElementById("SubmitCreds").InvokeMember("click");
-
-            foreach (HtmlElement tag in webBrowser1.Document.GetElementsByTagName("a"))
-            {
-                if (tag.GetAttribute("span") == "Campus Students")
-                {
-                    tag.InvokeMember("click");
-                }
-            }
-
-        }
-
-
-        /* After logging in to mysnhu, navigate to course offerings */
-        private void navigateToCourses()
-        {
-            webBrowser1.Navigate("https://my.snhu.edu/coursecatalog/Lists/Sections/SectionsBySubject.aspx?View={215C6024-AB45-46EF-B9CC-ADF871654DC5}&FilterField1=SectionLocation&FilterValue1=Manchester%20New%20Hampshire");
-        }
-
-
-        /* Show all computer science classes */
-        private void navigateToCompSciMajor()
-        {
-            webBrowser1.Document.GetElementById("img_1-15_").InvokeMember("click");
-            waitForCoursesToLoad();
-        }
-
-
-        /* Wait until classes are loaded, then save the data */
-        private async void waitForCoursesToLoad()
-        {
-            await Task.Delay(5000);
-            foreach (HtmlElement tag in webBrowser1.Document.GetElementById("tbod1-15__").Children)
-            {
-                if (tag.CanHaveChildren)
-                {
-                    foreach (HtmlElement childtag in tag.Children)
-                    {
-                        if (childtag.InnerText == null)
-                        {
-                            continue;
-                        }
-                        courseData += childtag.InnerText;
-                        courseData += "\n";
-                    }
-                    if (courseData == null) continue;
-                    SNHUcourse course = new SNHUcourse(courseData);
-                    courseList.Add(course);
-                }
-
-                courseData = "";
-                Console.WriteLine("\n\n\n");
-            }
-            webBrowser1.Dispose();
+            WebbrowserForm webForm = new WebbrowserForm();
+            webForm.ShowDialog();
         }
     }
 }
